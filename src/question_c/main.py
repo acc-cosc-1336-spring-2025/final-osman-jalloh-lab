89cc6683cd13ed7aade64562d5023d263c396aca
osman-jalloh-lab <osman.jalloh@g.austincc.edu>
2025-05-15 00:04:37 +0000

    question 3 or c

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,14 @@
-#add import
+from multiplication import create_multiplication_table, display_multiplication_table 
+def main():
+    while True:
+        table = create_multiplication_table()
+        print("\nMultiplication Table:")
+        display_multiplication_table(table)
+
+        choice = input("\nDo you want to continue? (y/n): ")
+        if choice.lower() != 'y':
+            print("Exiting program. Goodbye!")
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ src/question_c/multiplication.py
@@ -0,0 +1,12 @@
+def create_multiplication_table():
+    table = []
+    for i in range(1, 6): 
+        row = []
+        for j in range(1, 11): 
+            row.append(i * j)
+        table.append(row)
+    return table
+
+def display_multiplication_table(table):
+    for row in table:
+        print(" ".join(str(num).rjust(2) for num in row))
